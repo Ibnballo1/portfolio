9bb0c1143d8b8afbd96e1042c8d1a6eb1be2fbeb
Ibnballo1 <belloabdullateef035@gmail.com>
2022-05-09 21:55:55 +0100

    Add variables

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Mobile Menu
-const menuBar = document.getElementById('hamburger');
+const menuBar = document.getElementById('menu_bar');
 const closeBtn = document.getElementById('close_btn');
+const hamburgerBtn = document.getElementById('hamburger');
+const closeBar = document.getElementById('close_bar');
+const lists = document.getElementById('menu_list');
 
 menuBar.addEventListener('click', ()=> {
     menuBar.style.display = 'none';
