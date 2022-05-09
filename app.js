46617e31a55f8a544b70575746289301dfadd66c
Elson Otake <elsonotake@yahoo.com.br>
2022-05-09 17:40:45 -0300

    Update hidden hamburger icon

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const menuBar = document.getElementById('hamburger');
 const closeBtn = document.getElementById('close_btn');
 
 menuBar.addEventListener('click', ()=> {
-    
+    menuBar.style.display = 'none';
 })
