24beaf7da216ce28887376f4bb00b559b9d3b720
Elson Otake <elsonotake@yahoo.com.br>
2022-05-09 18:00:34 -0300

    Add more variables

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const closeBtn = document.getElementById('close_btn');
 const hamburgerBtn = document.getElementById('hamburger');
 const closeBar = document.getElementById('close_bar');
 const lists = document.getElementById('menu_list');
+const hiddenScrol = document.getElementById('hideOverflow');
+const blurEffect = document.querySelectorAll('.toblur');
+let isDisplay = true;
 
 menuBar.addEventListener('click', ()=> {
     menuBar.style.display = 'none';
