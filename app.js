fadf1729b2e965a9c31b4b938aea38d53eaa705c
Ibnballo1 <belloabdullateef035@gmail.com>
2022-05-10 17:53:59 +0100

    Fix linter errors

--- app.js
+++ app.js
@@ -1,40 +1,4 @@
 // Mobile Menu
-/*const menuBar = document.getElementById('menu_bar');
-const closeBtn = document.getElementById('close_btn');
-const hamburgerBtn = document.getElementById('hamburger');
-const closeBar = document.getElementById('close_bar');
-const lists = document.getElementById('menu_list');
-const hiddenScrol = document.getElementById('hideOverflow');
-const blurEffect = document.querySelectorAll('.toblur');
-let isDisplay = true;
-
-menuBar.addEventListener('click', ()=> {
-    if (isDisplay) {
-        hamburgerBtn.style.display = 'none';
-        closeBtn.style.display = 'block';
-        closeBar.classList.add('icon-menu');
-        lists.classList.add('js-nav-list');
-        hiddenScrol.classList.add('hide-scroll');
-    
-        for (let i = 0; i < blurEffect.length; i += 1) {
-          blurEffect[i].classList.add('blurring');
-        }
-    
-        isDisplay = false;
-      } else {
-        hamburgerBtn.style.display = 'block';
-        closeBtn.style.display = 'none';
-        closeBar.classList.replace('icon-menu', 'nav-menu');
-        lists.classList.replace('js-nav-list', 'dv-nav');
-        hiddenScrol.classList.remove('hide-scroll');
-    
-        for (let i = 0; i < blurEffect.length; i += 1) {
-          blurEffect[i].classList.remove('blurring');
-        }
-    
-        isDisplay = true;
-    }
-})*/
 
 const menuBar = document.getElementById('menu_bar');
 const times = document.getElementById('close_menu');
@@ -52,11 +16,9 @@ menuBar.addEventListener('click', () => {
     closeBar.classList.add('icon-menu');
     lists.classList.add('js-nav-list');
     hiddenScrol.classList.add('hide-scroll');
-
     for (let i = 0; i < blurEffect.length; i += 1) {
       blurEffect[i].classList.add('blurring');
     }
-
     isDisplay = false;
   } else {
     hamburgerBtn.style.display = 'block';
@@ -64,11 +26,9 @@ menuBar.addEventListener('click', () => {
     closeBar.classList.replace('icon-menu', 'nav-menu');
     lists.classList.replace('js-nav-list', 'dv-nav');
     hiddenScrol.classList.remove('hide-scroll');
-
     for (let i = 0; i < blurEffect.length; i += 1) {
       blurEffect[i].classList.remove('blurring');
     }
-
     isDisplay = true;
   }
 });
